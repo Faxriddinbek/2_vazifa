5a6f8aee6edde2df77aab3e855881ee4c748ac35
Faxriddinbek O'rinboyev <faxriddinorinboyev12@gmail.com>
2025-08-05 16:41:52 +0500

    Delete 18 - dars.py

--- 18 - dars.py
+++
@@ -1,7 +0,0 @@
-class Person:
-    def __init__(self, name, age, gender):
-        self.name = name   # atribut
-        self.age = age  # atribut
-        self.gender = gender # atribut
-doe = Person(name="Doe", age=12, gender="Erkak")
-print(f"Ismi {doe.name} Yoshi {doe.age} jinsi {doe.gender}")
